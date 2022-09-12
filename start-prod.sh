741605005fef433c557d7e40f40b38aa4b72d0a3
Tameem Bin Haider <tameem.haider@dsinnovators.com>
2022-09-12 14:18:02 +0600

    Wait for auth service to start

--- start-prod.sh
+++ start-prod.sh
@@ -13,4 +13,12 @@ set -e
 sed -i "s/{{hostname}}/$HOSTNAME/g" src/client-configs/client-config.prod.js
 sed -i "s/{{hostname}}/$HOSTNAME/g" src/client-configs/login-config.prod.js
 
+apt-get update
+apt-get install curl
+
+curl -L https://github.com/ufoscout/docker-compose-wait/releases/download/2.9.0/wait --output /wait
+chmod +x /wait
+
+WAIT_TIMEOUT=240 WAIT_HOSTS=auth:4040 /wait
+
 yarn start:prod
