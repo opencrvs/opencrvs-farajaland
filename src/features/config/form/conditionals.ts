d6b4f402af958e8cc393d661c8ddd66ff3f34135
naftis <pyry.rouvila@gmail.com>
2023-06-29 15:26:55 +0300

    add a example of isDefaultCountry

--- src/features/config/form/conditionals.ts
+++ src/features/config/form/conditionals.ts
@@ -1,20 +1,7 @@
-import { IntegratingSystemType } from './types'
-
 export const conditionals = {
   // This is an example how you can override the conditionals found from opencrvs-core
-  iDType: {
+  isDefaultCountry: {
     action: 'hide',
-    expression: "!values.iDType || (values.iDType !== 'OTHER')"
-  },
-  hideIfNidIntegrationDisabled: {
-    action: 'hide',
-    expression: `const nationalIdSystem =
-    offlineCountryConfig &&
-    offlineCountryConfig.systems.find(s => s.integratingSystemType === '${IntegratingSystemType.Mosip}');
-    !nationalIdSystem ||
-    !nationalIdSystem.settings.openIdProviderBaseUrl ||
-    !nationalIdSystem.settings.openIdProviderClientId ||
-    !nationalIdSystem.settings.openIdProviderClaims;
-  `
+    expression: 'isDefaultCountry(values.country)'
   }
 }
