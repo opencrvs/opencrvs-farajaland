bf6900d946ff9ec295211c8a3ef86dd86375f67b
Riku Rouvila <riku.rouvila@gmail.com>
2023-11-29 14:03:03 +0200

    Setup new alerts: SSH login, error in backup logs, available disk space in data partition

--- infrastructure/monitoring/kibana/setup-config.sh
+++ infrastructure/monitoring/kibana/setup-config.sh
@@ -7,14 +7,21 @@
 #
 # Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
 
+#!/bin/bash
 set -e
-curl --insecure --connect-timeout 60 -u elastic:${ELASTICSEARCH_SUPERUSER_PASSWORD} -X POST https://$HOST/api/saved_objects/_import?overwrite=true -H 'kbn-xsrf: true' --form file=@/opt/opencrvs/infrastructure/monitoring/kibana/config.ndjson > /dev/null
 
-curl --insecure --connect-timeout 60 -u elastic:${ELASTICSEARCH_SUPERUSER_PASSWORD} \
-https://$HOST/api/alerting/rules/_find\?page\=1\&per_page\=100\&default_search_operator\=AND\&sort_field\=name\&sort_order\=asc | \
-jq -r '.data[].id' | \
-while read -r id; do
-  echo "$id";
-  curl --insecure --connect-timeout 60 -X POST -H 'kbn-xsrf: true' -u elastic:${ELASTICSEARCH_SUPERUSER_PASSWORD} "https://$HOST/api/alerting/rule/$id/_disable"
-  curl --insecure --connect-timeout 60 -X POST -H 'kbn-xsrf: true' -u elastic:${ELASTICSEARCH_SUPERUSER_PASSWORD} "https://$HOST/api/alerting/rule/$id/_enable"
+# Define Docker command with the image and network
+docker_command="docker run --rm -v /opt/opencrvs/infrastructure/monitoring/kibana/config.ndjson:/config.ndjson --network=opencrvs_overlay_net curlimages/curl"
+
+# First delete all alerts. This is because the import doesn't remove alerts that are no longer in the config
+$docker_command --connect-timeout 60 -u elastic:${ELASTICSEARCH_SUPERUSER_PASSWORD} http://kibana:5601/api/alerting/rules/_find\?page\=1\&per_page\=100\&default_search_operator\=AND\&sort_field\=name\&sort_order\=asc | docker run --rm -i --network=opencrvs_overlay_net stedolan/jq -r '.data[].id' | while read -r id; do
+  $docker_command --connect-timeout 60 -X DELETE -H 'kbn-xsrf: true' -u elastic:${ELASTICSEARCH_SUPERUSER_PASSWORD} "http://kibana:5601/api/alerting/rule/$id"
+done
+
+$docker_command --connect-timeout 60 -u elastic:${ELASTICSEARCH_SUPERUSER_PASSWORD} -X POST http://kibana:5601/api/saved_objects/_import?overwrite=true -H 'kbn-xsrf: true' --form file=@/config.ndjson > /dev/null
+
+# Re-enable all alerts. This is because after importing a config, all alerts are disabled by default
+$docker_command --connect-timeout 60 -u elastic:${ELASTICSEARCH_SUPERUSER_PASSWORD} http://kibana:5601/api/alerting/rules/_find\?page\=1\&per_page\=100\&default_search_operator\=AND\&sort_field\=name\&sort_order\=asc | docker run --rm -i --network=opencrvs_overlay_net stedolan/jq -r '.data[].id' | while read -r id; do
+  $docker_command --connect-timeout 60 -X POST -H 'kbn-xsrf: true' -u elastic:${ELASTICSEARCH_SUPERUSER_PASSWORD} "http://kibana:5601/api/alerting/rule/$id/_disable"
+  $docker_command --connect-timeout 60 -X POST -H 'kbn-xsrf: true' -u elastic:${ELASTICSEARCH_SUPERUSER_PASSWORD} "http://kibana:5601/api/alerting/rule/$id/_enable"
 done
