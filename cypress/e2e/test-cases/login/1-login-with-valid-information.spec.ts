6df0ce9224ebb4720d5cd13eb7f01f99e6cf23da
Riku Rouvila <riku.rouvila@gmail.com>
2024-05-02 16:20:21 +0300

    add some initial structure for test cases

---
+++ cypress/e2e/test-cases/login/1-login-with-valid-information.spec.ts
@@ -0,0 +1,15 @@
+describe('1. Login with valid information', () => {
+  before(() => {
+    // Initialise the test environment
+  })
+  it('1.1. Navigate to the login URL', () => {
+    // Expected result: User should redirect to the login page
+  })
+  it('1.2. Enter your user name and Password', () => {
+    // Expected result: User should navigate to the Next page to verify through Mobile number or Email address.
+  })
+  describe('1.3. Navigate to the login URL', () => {
+    it.skip('Validate the SMS for 2fa', () => {})
+    it('Validate the email for 2fa', () => {})
+  })
+})
