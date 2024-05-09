40222b50cf4f6e24e591b27c2f54270311b7878c
Riku Rouvila <riku.rouvila@gmail.com>
2024-05-09 18:55:45 +0300

    try nesting tests more with step

--- e2e/testcases/birth/1-birth-event-declaration.spec.ts
+++ e2e/testcases/birth/1-birth-event-declaration.spec.ts
@@ -15,43 +15,41 @@ test.describe('1. Birth event declaration', () => {
   }) => {
     await page.click('#header_new_event')
     await page.waitForSelector('#continue')
-  })
 
-  test('1.2. Validate event selection page', async ({ page }) => {
-    await page.click('#header_new_event')
+    await test.step('1.2. Validate event selection page', async () => {
+      await test.step('1.2.1 Validate the contents of the event type page', async () => {
+        /*
+         * Expected result: should show
+         * - Radio buttons of the events
+         * - Continue button
+         * - Exit button
+         */
+        await expect(page.locator('#select_birth_event')).toBeVisible()
+        await expect(page.locator('#select_death_event')).toBeVisible()
+        await expect(page.locator('#select_marriage_event')).toBeVisible()
+        await expect(page.locator('#goBack')).toBeVisible()
+        await expect(page.locator('#continue')).toBeVisible()
+      })
 
-    await test.step('1.2.1 Validate the contents of the event type page', async () => {
-      /*
-       * Expected result: should show
-       * - Radio buttons of the events
-       * - Continue button
-       * - Exit button
-       */
-      await expect(page.locator('#select_birth_event')).toBeVisible()
-      await expect(page.locator('#select_death_event')).toBeVisible()
-      await expect(page.locator('#select_marriage_event')).toBeVisible()
-      await expect(page.locator('#goBack')).toBeVisible()
-      await expect(page.locator('#continue')).toBeVisible()
-    })
+      await test.step('1.2.2 Click the "Continue" button without selecting any event', async () => {
+        await page.click('#continue')
+        /*
+         * Expected result: should throw an error as below:
+         * "Please select the type of event"
+         */
+        await expect(page.locator('#require-error')).toBeVisible()
+      })
 
-    await test.step('1.2.2 Click the "Continue" button without selecting any event', async () => {
-      await page.click('#continue')
-      /*
-       * Expected result: should throw an error as below:
-       * "Please select the type of event"
-       */
-      await expect(page.locator('#require-error')).toBeVisible()
-    })
-
-    await test.step('1.2.3 Select the "Birth" event and click "Continue" button', async () => {
-      await page.click('#select_birth_event')
-      await page.click('#continue')
-      /*
-       * Expected result: User should navigate to the "Introduction" page
-       */
-      await expect(
-        page.locator('#form_section_id_information-group')
-      ).toBeVisible()
+      await test.step('1.2.3 Select the "Birth" event and click "Continue" button', async () => {
+        await page.click('#select_birth_event')
+        await page.click('#continue')
+        /*
+         * Expected result: User should navigate to the "Introduction" page
+         */
+        await expect(
+          page.locator('#form_section_id_information-group')
+        ).toBeVisible()
+      })
     })
   })
 })
