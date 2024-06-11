949eb2f0b5e13f3358a4a2b46f91d43d8054bc8f
Eezi <tomi.a.salo@outlook.com>
2024-06-11 19:53:07 +0300

    Added familyName validation tests

--- e2e/testcases/marriage/7-validate-witness-1-page.spec.ts
+++ e2e/testcases/marriage/7-validate-witness-1-page.spec.ts
@@ -14,10 +14,8 @@ test.describe('7. Validate Witness 1 details page', () => {
     await page.getByText('Continue', { exact: true }).click()
   })
 
-  // TODO: Jaa nämä testit pieniempiin kokonaisuuksiin.
-  test('1. Validate "First Name(s)" text field', async ({ page }) => {
-    // label: "First name(s)" input id: "#firstNamesEng"
-    // 1.1. Enter Non-English characters
+  // 1.1. Enter Non-English characters
+  test('1.1. Validate "First Name(s)" text field', async ({ page }) => {
     await page.locator('#firstNamesEng').fill('*')
     await expect(
       page.getByText(
@@ -25,59 +23,89 @@ test.describe('7. Validate Witness 1 details page', () => {
         { exact: true }
       )
     ).toBeVisible()
+  })
 
-    // 1.2. Enter less than 33 English characters
-    // There is no such an error case.
+  // 1.2. Enter less than 33 English characters
+  test('1.2. Enter less than 33 English characters', async ({ page }) => {
+    await page.locator('#firstNamesEng').fill('Rakibul Islam')
+    await page.getByText('Birth declaration').click()
 
-    // 1.3. Enter Field as NULL
+    await expect(page.locator('#firstNamesEng_error')).toBeHidden()
+  })
+
+  // 1.3. Enter Field as NULL
+  test('1.3. Enter Field as NULL', async ({ page }) => {
     await page.locator('#firstNamesEng').click()
     await page.locator('#relationship').click()
     await expect(
       page.getByText('Required for registration', { exact: true })
     ).toBeVisible()
-
-    // 1.4. Enter more than 32 English characters
-    // There is no such an error case and the input field maxLength is 32.
   })
 
-  test('2. Validate "First Name(s)" text field', async ({ page }) => {
-    // label: "Last name" input id: "#familyNameEng"
-    // 2.1. Enter Non-English characters
-    await page.locator('#familyNameEng').fill('O’Neill')
-    await expect(
-      page.getByText(
-        `Input contains invalid characters. Please use only letters (a-z, A-Z), numbers (0-9), hyphens (-), apostrophes(') and underscores (_)`,
-        { exact: true }
-      )
-    ).toBeVisible()
+  // 1.4. Enter more than 32 English characters
+  test('1.4. Enter more than 32 English characters', async ({ page }) => {
+    const LONG_NAME = 'Ovuvuevuevue Enyetuenwuevue Ugbemugbem Osas'
+    await page.locator('#firstNamesEng').fill(LONG_NAME)
+    await page.getByText('Witness 1 details').click()
 
-    // 2.2. Enter less than 33 English characters
-    // There is no such an error case.
+    await expect(page.locator('#firstNamesEng')).toHaveValue(
+      LONG_NAME.slice(0, 32)
+    )
+  })
+})
 
-    // 2.3. Enter Field as NULL
-    await page.locator('#familyNameEng').click()
-    await page.locator('#relationship').click()
-    await expect(
-      page.getByText('Required for registration', { exact: true })
-    ).toBeVisible()
+// 2.1. Enter Non-English characters
+test('2.1. Validate "Last Name(s)" text field', async ({ page }) => {
+  await page.locator('#familyNameEng').fill('O’Neill')
+  await expect(
+    page.getByText(
+      `Input contains invalid characters. Please use only letters (a-z, A-Z), numbers (0-9), hyphens (-), apostrophes(') and underscores (_)`,
+      { exact: true }
+    )
+  ).toBeVisible()
+})
 
-    // 2.4. Enter more than 32 English characters
-    // There is no such an error case and the input field maxLength is 32.
-  })
-  // Done
-  test('3. Select any to the following option from Relationship to spouses:', async ({
-    page
-  }) => {
-    // label: "Relationship to spouses" input id: "#relationship"
-    await page.locator('#relationship').click()
-    await page.getByText('Other', { exact: true }).click()
-    await expect(page.getByText('Other', { exact: true })).toBeVisible()
-  })
-  // Done
-  test('4. Click continue', async ({ page }) => {
-    await page.getByText('Continue', { exact: true }).click()
-    await expect(
-      page.getByText('Witness 2 details', { exact: true })
-    ).toBeVisible()
-  })
+// 2.2. Enter less than 33 English characters
+test('2.2. Enter less than 33 English characters', async ({ page }) => {
+  await page.locator('#familyNamesEng').fill('Rakibul Islam')
+  await page.getByText('Witness 1 details').click()
+
+  await expect(page.locator('#familyNamesEng_error')).toBeHidden()
+})
+
+// 2.3. Enter Field as NULL
+test('2.3. Enter Field as NULL', async ({ page }) => {
+  await page.locator('#familyNameEng').click()
+  await page.locator('#relationship').click()
+  await expect(
+    page.getByText('Required for registration', { exact: true })
+  ).toBeVisible()
+})
+
+// 2.4. Enter more than 32 English characters
+test('2.4. Enter more than 32 English characters', async ({ page }) => {
+  const LONG_NAME = 'Ovuvuevuevue Enyetuenwuevue Ugbemugbem Osas'
+  await page.locator('#familyNamesEng').fill(LONG_NAME)
+  await page.getByText('Witness 1 details').click()
+
+  await expect(page.locator('#familyNamesEng')).toHaveValue(
+    LONG_NAME.slice(0, 32)
+  )
+})
+// Done
+test('3. Select any to the following option from Relationship to spouses:', async ({
+  page
+}) => {
+  // label: "Relationship to spouses" input id: "#relationship"
+  await page.locator('#relationship').click()
+  await page.getByText('Other', { exact: true }).click()
+  await expect(page.getByText('Other', { exact: true })).toBeVisible()
+})
+// Done
+test('4. Click continue', async ({ page }) => {
+  await page.getByText('Continue', { exact: true }).click()
+  await expect(
+    page.getByText('Witness 2 details', { exact: true })
+  ).toBeVisible()
+})
 })
