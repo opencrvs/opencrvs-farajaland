18597cfa82621f2efd55f3fd7d3a05a53ee5e392
AshwinMN24 <ashwin@opencrvs.org>
2024-09-17 22:57:22 +0530

    multiple images pull concurrently

--- infrastructure/deployment/deploy.sh
+++ infrastructure/deployment/deploy.sh
@@ -289,12 +289,11 @@ docker_stack_deploy() {
   done
   cat docker_images.txt
   echo "The current working directory is $PWD"
-  ls infrastructure
-  #find / -name "multiple-images-pull.py"
-  python3 --version
+  ls infrastructure/deployment/
+  echo "------"
   ls -la /opt
   #mkdir /opt/opencrvs
-  #cp infrastructure/deployment/multiple-images-pull.py /opt/opencrvs/multiple-images-pull.py
+  cp infrastructure/deployment/multiple-images-pull.py /opt/opencrvs/multiple-images-pull.py
   #cp docker_images.txt /opt/opencrvs/docker_images.txt
   current_dir=$(pwd)
   configured_ssh "cd /opt/opencrvs && python3 $current_dir/infrastructure/deployment/multiple-images-pull.py"
