028a24c637c74321fa1233a4d0e40bf5a935b770
AshwinMN24 <ashwin@opencrvs.org>
2024-09-18 22:03:21 +0530

    multiple downloads

--- infrastructure/deployment/deploy.sh
+++ infrastructure/deployment/deploy.sh
@@ -289,7 +289,7 @@ docker_stack_deploy() {
     do
       echo "Server failed to download $tag. Retrying..."
       sleep 5
-    done
+    done &
   done
 
   echo "Updating docker swarm stack with new compose files"
