969eac147b0ed48def20ea87d614ef5eecf7a29f
jamil314 <jamil31415926@gmail.com>
2024-10-21 16:52:50 +0600

    Revert "feat:e2e: capture screenshot on failure"
    This reverts commit 89b529d6aa22acd2d49f7045bc4e2af8ecc60741 because it had some extra changes.

--- e2e/constants.ts
+++ e2e/constants.ts
@@ -1,9 +1,4 @@
-export const DOMAIN = process.env.DOMAIN || 'action-menu.opencrvs.dev'
-// export const DOMAIN = process.env.DOMAIN || 'farajaland-dev.opencrvs.org'
-// export const LOGIN_URL = 'http://localhost:3020'
-// export const AUTH_URL = 'http://localhost:4040'
-// export const CLIENT_URL = 'http://localhost:3000'
-// export const GATEWAY_HOST = 'http://localhost:7070'
+export const DOMAIN = process.env.DOMAIN || 'farajaland-dev.opencrvs.org'
 export const LOGIN_URL = 'https://login.' + DOMAIN
 export const AUTH_URL = 'https://auth.' + DOMAIN
 export const CLIENT_URL = 'https://register.' + DOMAIN
