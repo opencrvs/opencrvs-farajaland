331ccc062256af1fb98450a46f22fa164f88917c
tareq89 <tareq.aziz@dsinnovators.com>
2024-10-22 18:43:43 +0600

    selectedTemplate translation added

--- src/form/types/types.ts
+++ src/form/types/types.ts
@@ -791,7 +791,7 @@ export type IDeclarationCertificate = {
   corrector?: Partial<{ type: RelationForCertificateCorrection }>
   hasShowedVerifiedDocument?: boolean
   payments?: Payment
-  data?: string
+  certTemplateId?: string
 }
 
 export interface IFileValue {
