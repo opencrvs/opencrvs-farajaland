89032bdd54a1b1672789d21a8800dafb8adb8e9e
cibelius <cihan.m.bebek@gmail.com>
2025-05-13 14:37:24 +0300

    Revert "disable flaky test"
    This reverts commit 399690d56397d1412a2bf7c58269e40e7c194f0f.

--- e2e/testcases/applications/17-validate-user-can-correct-a-record-from-audit-record-page.spec.ts
+++ e2e/testcases/applications/17-validate-user-can-correct-a-record-from-audit-record-page.spec.ts
@@ -14,7 +14,6 @@ import { CREDENTIALS } from '../../constants'
 test.describe
   .serial('17. Validate user can correct a record from audit record page', () => {
   let page: Page
-
   test.beforeAll(async ({ browser }) => {
     page = await browser.newPage()
   })
@@ -22,7 +21,6 @@ test.describe
   test.afterAll(async () => {
     await page.close()
   })
-
   test('17.0 Create Declaration', async () => {
     const token = await getToken('k.mweene', 'test')
     const res = await createDeclaration(token, {
@@ -86,8 +84,7 @@ test.describe
     expect(page.url().includes('record-audit'))
   })
 
-  // @TODO: This test has been causing flakyness on the CI pipeline
-  test.fixme('17.2 Click download > click assign', async () => {
+  test('17.2 Click download > click assign', async () => {
     await assignRecord(page)
     await page.getByRole('button', { name: 'Action' }).first().click()
 
@@ -107,8 +104,7 @@ test.describe
     )
   })
 
-  // @TODO: This test has been causing flakyness on the CI pipeline
-  test.fixme('17.3 Click "Correct record"', async () => {
+  test('17.3 Click "Correct record"', async () => {
     await getAction(page, 'Correct record').click()
 
     /*
