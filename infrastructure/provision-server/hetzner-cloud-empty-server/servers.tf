bce3ab71e9ea15947e3454df5d71d2fedecc27a0
Vadym Mudryi <vadym.mudryi@gmail.com>
2025-05-23 13:25:22 +0300

    testing

--- infrastructure/provision-server/hetzner-cloud-empty-server/servers.tf
+++ infrastructure/provision-server/hetzner-cloud-empty-server/servers.tf
@@ -5,12 +5,11 @@ module "opencrvs_single_node" {
   source             = "./single-node"
   name               = "${var.country_name}-${var.env_name}"
   # private_network_id = hcloud_network.private_network.id
-  location = "hel1"
+  location = "fsn1"
   server_type = "cpx41"
   user_data = templatefile("cloud-init.yaml", {
     ssh_key = trimspace(file(".ssh/id_rsa.pub"))
   })
-
 }
 
 module "opencrvs_multi_node" {
