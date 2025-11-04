aebc6aa5733df31f3220b34693180f8089230958
jamil314 <jamil31415926@gmail.com>
2025-11-04 14:37:56 +0600

    e2e: add testcases for showing searchbar based on scopes

---
+++ e2e/testcases/v2-quick-search/searchbar-visility-by-scope.spec.ts
@@ -0,0 +1,48 @@
+import { test, expect } from '@playwright/test'
+import { CREDENTIALS } from '../../constants'
+import { loginToV2 } from '../../helpers'
+const testCases = [
+  {
+    credential: CREDENTIALS.FIELD_AGENT,
+    hasSearch: true
+  },
+  {
+    credential: CREDENTIALS.REGISTRATION_AGENT,
+    hasSearch: true
+  },
+  {
+    credential: CREDENTIALS.LOCAL_REGISTRAR,
+    hasSearch: true
+  },
+  {
+    credential: CREDENTIALS.NATIONAL_REGISTRAR,
+    hasSearch: true
+  },
+  {
+    credential: CREDENTIALS.NATIONAL_SYSTEM_ADMIN,
+    hasSearch: false
+  },
+  {
+    credential: CREDENTIALS.LOCAL_SYSTEM_ADMIN,
+    hasSearch: false
+  },
+  {
+    credential: CREDENTIALS.PERFORMANCE_MANAGER,
+    hasSearch: false
+  }
+]
+test.describe('Search bar should be visible only if the user has search scope', () => {
+  for (const { credential, hasSearch } of testCases) {
+    test(`${credential.USERNAME} ${hasSearch ? 'has' : 'does not have'} search scope`, async ({
+      page
+    }) => {
+      await loginToV2(page, credential)
+
+      if (hasSearch) {
+        await expect(page.locator('#searchText')).toBeVisible()
+      } else {
+        await expect(page.locator('#searchText')).not.toBeVisible()
+      }
+    })
+  }
+})
