4c3be450094326c2e0f3fa70a38156b9f8362406
Barry Dwyer <barry.dwyer@decodedev.co.za>
2026-01-06 12:22:50 +0200

    Add registrationNumber field to tennis club search so it can be tested

--- src/form/tennis-club-membership.ts
+++ src/form/tennis-club-membership.ts
@@ -1283,6 +1283,7 @@ export const tennisClubMembershipEvent = defineConfig({
         id: 'advancedSearch.form.registrationDetails'
       },
       fields: [
+        event('legalStatuses.REGISTERED.registrationNumber').exact(),
         event('legalStatuses.REGISTERED.createdAtLocation').within(),
         event('legalStatuses.REGISTERED.acceptedAt').range(),
         event('status').exact(),
