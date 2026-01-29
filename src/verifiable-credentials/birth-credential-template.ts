769d86e8a6115b45283ca5173a76f9616c920125
naftis <pyry.rouvila@gmail.com>
2026-01-29 15:36:59 +0200

    define the vc a bit further

--- src/verifiable-credentials/birth-credential-template.ts
+++ src/verifiable-credentials/birth-credential-template.ts
@@ -1,5 +1,6 @@
 import { logger } from '@countryconfig/logger'
 import { EventIndex, NameFieldValue } from '@opencrvs/toolkit/events'
+import type { BirthCredentialData } from './birth-credential-definition'
 
 export const birthCredentialTemplate = (event: EventIndex) => {
   console.log(JSON.stringify(event, null, 2))
@@ -9,7 +10,15 @@ export const birthCredentialTemplate = (event: EventIndex) => {
   )
 
   const childName = event.declaration['child.name'] as NameFieldValue
+  const fatherName = event.declaration['father.name'] as
+    | NameFieldValue
+    | undefined
+  const motherName = event.declaration['mother.name'] as
+    | NameFieldValue
+    | undefined
   const dateOfEvent = event.dateOfEvent as string
+  const childGender =
+    (event.declaration['child.gender'] as string) === 'male' ? 1 : 2
 
   return {
     issuerKey: {
@@ -26,10 +35,36 @@ export const birthCredentialTemplate = (event: EventIndex) => {
     credentialConfigurationId: 'crvs_birth_v1',
     credentialData: {
       given_name: childName.firstname,
+      middle_name: childName.middlename,
       family_name: childName.surname,
       birthdate: dateOfEvent,
-      place_of_birth: 'Chamakubi Health Post, Ibombo, Central, Farajaland'
-    },
+
+      place_of_birth: {
+        name: 'Chamakubi Health Post, Ibombo, Central',
+        country: 'FAR'
+      },
+
+      nationalities: ['FAR'],
+
+      sex: childGender,
+
+      parents: [
+        {
+          given_name: motherName?.firstname,
+          middle_name: motherName?.middlename,
+          family_name: motherName?.surname,
+          identifier: event.declaration['mother.nid'] as string | undefined,
+          nationalities: ['FAR']
+        },
+        {
+          given_name: fatherName?.firstname,
+          middle_name: fatherName?.middlename,
+          family_name: fatherName?.surname,
+          identifier: event.declaration['father.nid'] as string | undefined,
+          nationalities: ['FAR']
+        }
+      ]
+    } satisfies BirthCredentialData,
     mapping: {
       id: '<uuid>',
       iat: '<timestamp-seconds>',
@@ -39,14 +74,15 @@ export const birthCredentialTemplate = (event: EventIndex) => {
     authenticationMethod: 'PRE_AUTHORIZED',
     selectiveDisclosure: {
       fields: {
+        given_name: { sd: true },
+        middle_name: { sd: true },
+        family_name: { sd: true },
         birthdate: {
           sd: true
         },
         place_of_birth: {
           sd: true
-        },
-        given_name: { sd: true },
-        family_name: { sd: true }
+        }
       },
       decoyMode: 'NONE',
       decoys: 0
