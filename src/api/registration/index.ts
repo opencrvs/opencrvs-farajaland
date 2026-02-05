48ba2c75c77cb9d2b4f292dc1a849b7c44c91b85
naftis <pyry.rouvila@gmail.com>
2026-02-05 17:10:56 +0200

    first working version of packet manager on Collab :)

--- src/api/registration/index.ts
+++ src/api/registration/index.ts
@@ -27,6 +27,7 @@ import {
   shouldForwardBirthRegistrationToMosip,
   shouldForwardDeathRegistrationToMosip
 } from '@countryconfig/form/v2/mosip'
+import { capitalize } from 'lodash'
 
 export interface ActionConfirmationRequest extends Hapi.Request {
   payload: EventDocument
@@ -187,23 +188,61 @@ export async function onMosipBirthRegisterHandler(
       trackingId: event.trackingId,
       requestFields: {
         birthCertificateNumber: registrationNumber,
-        fullName: [
-          childName?.firstname,
-          childName?.middlename,
-          childName?.surname
-        ]
-          .filter(Boolean)
-          .join(' '),
-        dateOfBirth: declaration['child.dob'],
-        gender: declaration['child.gender']
+        fullName:
+          '[ {\n  "language" : "eng",\n  "value" : "' +
+          [childName?.firstname, childName?.middlename, childName?.surname]
+            .filter(Boolean)
+            .join(' ') +
+          '"\n}]',
+
+        dateOfBirth: declaration['child.dob']?.toString().replaceAll('-', '/'),
+        gender:
+          '[ {\n  "language" : "eng",\n  "value" : "' +
+          capitalize(declaration['child.gender'] as string) +
+          '"\n}]',
+        postalCode: '14022',
+        email: 'rachik.sharma@gmail.com',
+        phone: '7790075085',
+        zone: '[ {\n  "language" : "eng",\n  "value" : "Ben Mansour"\n}]',
+        region:
+          '[ {\n  "language" : "eng",\n  "value" : "Rabat Sale Kenitra"\n}]',
+        province: '[ {\n  "language" : "eng",\n  "value" : "Kenitra"\n}]',
+        preferredLang: 'English'
       },
       notification: {
-        recipientEmail: '@TODO',
-        recipientFullName: '@TODO',
-        recipientPhone: '@TODO'
+        recipientEmail: 'rachik.sharma@gmail.com',
+        recipientFullName: 'Rachik Sharma',
+        recipientPhone: '7790075085'
       },
-      metaInfo: {},
-      audit: {}
+      metaInfo: {
+        metaData:
+          '[{\n  "label" : "registrationType",\n  "value" : "NEW"\n}, {\n  "label" : "machineId",\n  "value" : "20042"\n}, {\n  "label" : "centerId",\n  "value" : "10001"\n}]',
+        registrationId: '10001100620007420250522121835',
+        operationsData:
+          '[ {\n  "label" : "officerId",\n  "value" : "crvs1"\n}, {\n  "label" : "officerBiometricFileName",\n  "value" : null\n}, {\n  "label" : "supervisorId",\n  "value" : null\n}, {\n  "label" : "supervisorBiometricFileName",\n  "value" : null\n}, {\n  "label" : "supervisorPassword",\n  "value" : "false"\n}, {\n  "label" : "officerPassword",\n  "value" : "true"\n}, {\n  "label" : "supervisorPIN",\n  "value" : null\n}, {\n  "label" : "officerPIN",\n  "value" : null\n}, {\n  "label" : "supervisorOTPAuthentication",\n  "value" : "false"\n}, {\n  "label" : "officerOTPAuthentication",\n  "value" : "false"\n} ]',
+        capturedRegisteredDevices: '[]',
+        creationDate: '20250225110733'
+      },
+      audit: {
+        uuid: 'c75s4521-87d6-6a4x-balw-2432e2355440',
+        createdAt: '2025-02-25T13:22:49.214Z',
+        eventId: 'REG-EVT-066',
+        eventName: 'PACKET_CREATION_SUCCESS',
+        eventType: 'USER',
+        hostName: 'DESKTOP-JL4BAEV',
+        hostIp: 'localhost',
+        applicationId: 'REG',
+        applicationName: 'REGISTRATION',
+        sessionUserId: 'crvs',
+        sessionUserName: 'crvs',
+        id: '10001100620007420250522121835',
+        idType: 'REGISTRATION_ID',
+        createdBy: 'crvs',
+        moduleName: 'Packet Handler',
+        moduleId: 'REG-MOD-117',
+        description: 'Packet Succesfully Created',
+        actionTimeStamp: '2025-02-25T07:52:49.214Z'
+      }
     })
 
     return h.response().code(202)
@@ -264,9 +303,9 @@ export async function onMosipDeathRegisterHandler(
         nationalIdNumber: declaration['deceased.nid']
       },
       notification: {
-        recipientEmail: '@TODO',
-        recipientFullName: '@TODO',
-        recipientPhone: '@TODO'
+        recipientEmail: 'rachik.sharma@gmail.com',
+        recipientFullName: 'Rachik Sharma',
+        recipientPhone: '+919999999999'
       },
       metaInfo: {},
       audit: {}
