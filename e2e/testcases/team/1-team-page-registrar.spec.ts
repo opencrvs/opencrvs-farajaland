3b1e95283a4b64438e185fe3eb6d9c9d520a8cbe
Markus <markus@agilepursuit.fi>
2026-02-16 12:42:34 +0200

    fix: remove unnecessary login

--- e2e/testcases/team/1-team-page-registrar.spec.ts
+++ e2e/testcases/team/1-team-page-registrar.spec.ts
@@ -15,7 +15,6 @@ test.describe.serial('1. Team Page -1', () => {
 
   test.describe('1.1 Team Page', async () => {
     test('1.1.0 Verify UI', async () => {
-      await login(page, CREDENTIALS.REGISTRAR)
       await page.getByRole('button', { name: 'Team' }).click()
       await expect(page.locator('#content-name')).toHaveText(
         'Ibombo District Office'
@@ -28,40 +27,24 @@ test.describe.serial('1. Team Page -1', () => {
       ).toBeVisible()
     })
 
-    test.describe('1.1 Team Page', async () => {
-      test('1.1.0 Verify UI', async () => {
-        await login(page, CREDENTIALS.REGISTRAR)
-        await page.getByRole('button', { name: 'Team' }).click()
-        await expect(page.locator('#content-name')).toHaveText(
-          'Ibombo District Office'
-        )
+    const team = [
+      { name: 'Mitchell Owen', role: 'Provincial Registrar' },
+      { name: 'Emmanuel Mayuka', role: 'Administrator' },
+      { name: 'Kennedy Mweene', role: 'Registrar' },
+      { name: 'Felix Katongo', role: 'Registration Officer' },
+      { name: 'Kalusha Bwalya', role: 'Hospital Official' }
+    ]
 
-        await expect(
-          page.getByText('Ibombo, Central', {
-            exact: true
-          })
-        ).toBeVisible()
-      })
+    test('1.1.1 Verify Team Members, Roles and their statuses', async () => {
+      const rows = page.locator('#user_list tr:has(td)')
+      await expect(rows).toHaveCount(team.length)
 
-      const team = [
-        { name: 'Mitchell Owen', role: 'Provincial Registrar' },
-        { name: 'Emmanuel Mayuka', role: 'Administrator' },
-        { name: 'Kennedy Mweene', role: 'Registrar' },
-        { name: 'Felix Katongo', role: 'Registration Officer' },
-        { name: 'Kalusha Bwalya', role: 'Hospital Official' }
-      ]
-
-      test('1.1.1 Verify Team Members, Roles and their statuses', async () => {
-        const rows = page.locator('#user_list tr:has(td)')
-        await expect(rows).toHaveCount(team.length)
-
-        for (let i = 0; i < team.length; i++) {
-          const cells = rows.nth(i).locator('td')
-          await expect(cells.nth(1)).toHaveText(team[i].name)
-          await expect(cells.nth(2)).toHaveText(team[i].role)
-          await expect(cells.nth(3)).toHaveText('Active')
-        }
-      })
+      for (let i = 0; i < team.length; i++) {
+        const cells = rows.nth(i).locator('td')
+        await expect(cells.nth(1)).toHaveText(team[i].name)
+        await expect(cells.nth(2)).toHaveText(team[i].role)
+        await expect(cells.nth(3)).toHaveText('Active')
+      }
     })
   })
 })
