a1fd9cef6750f19e3cc0cde37101ab735f92fb50
cibelius <cihan.m.bebek@gmail.com>
2026-03-26 09:50:23 +0200

    minor refactor

--- e2e/testcases/custom-actions-and-flags/revoke-and-reinstate.spec.ts
+++ e2e/testcases/custom-actions-and-flags/revoke-and-reinstate.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test, type Page } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 import { getToken, login, searchFromSearchBar } from '../../helpers'
 import { CREDENTIALS } from '../../constants'
 import { ensureAssigned, ensureOutboxIsEmpty, selectAction } from '../../utils'
