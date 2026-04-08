b4d2476601139cd8347e0a81e42fa17402b85391
jamil314 <jamil31415926@gmail.com>
2026-04-08 12:40:34 +0600

    fix: remove workflow env variable

--- src/environment.ts
+++ src/environment.ts
@@ -21,9 +21,6 @@ export const env = cleanEnv(process.env, {
   COUNTRY_CONFIG_URL: url({ devDefault: 'http://localhost:3040' }),
   APPLICATION_CONFIG_URL: url({ devDefault: 'http://localhost:2021/' }),
   SENTRY_DSN: str({ default: undefined }),
-  CONFIRM_REGISTRATION_URL: url({
-    devDefault: 'http://localhost:5050/confirm/registration'
-  }),
   QA_ENV: bool({ default: false }),
   ANALYTICS_DATABASE_URL: url({
     devDefault:
