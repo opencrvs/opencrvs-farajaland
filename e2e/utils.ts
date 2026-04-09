0021423f83d97a4c318daa25d991769a67320052
Markus <markus@agilepursuit.fi>
2026-04-09 12:25:28 +0300

    chore: assign explicitly on ensureAssigned
    Goal is to see how many tests depends on unassign retry logic

--- e2e/utils.ts
+++ e2e/utils.ts
@@ -77,50 +77,18 @@ export async function selectAction(
 }
 
 export async function ensureAssigned(page: Page) {
-  await page.waitForTimeout(SAFE_INPUT_CHANGE_TIMEOUT_MS)
-
   await page.getByRole('button', { name: 'Action', exact: true }).click()
 
-  const unAssignAction = page
-    .locator('#action-Dropdown-Content li')
-    .filter({ hasText: new RegExp(`^Unassign$`, 'i') })
-    .first()
-
-  let assignAction = page
+  const assignAction = page
     .locator('#action-Dropdown-Content li')
     .filter({ hasText: new RegExp(`^Assign$`, 'i') })
     .first()
 
-  // Wait until either "Unassign" or "Assign" is visible
-  await Promise.race([
-    unAssignAction.waitFor({ state: 'visible' }),
-    assignAction.waitFor({ state: 'visible' })
-  ])
+  assignAction.waitFor({ state: 'visible' })
 
-  if (await unAssignAction.isVisible()) {
-    await unAssignAction.click()
-    // Wait for the unassign modal to appear
-    await page.getByRole('button', { name: 'Unassign', exact: true }).click()
-    await expect(
-      page.getByRole('button', { name: 'Assign record' })
-    ).toBeVisible({ timeout: SAFE_OUTBOX_TIMEOUT_MS })
-
-    await page.getByRole('button', { name: 'Action', exact: true }).click()
-
-    // Seems that on slower environments actions are not immediately available after unassign.
-    // And end up having false negative for assignAction.isVisible().
-    await page.waitForTimeout(SAFE_INPUT_CHANGE_TIMEOUT_MS)
-    assignAction = page
-      .locator('#action-Dropdown-Content li')
-      .filter({ hasText: new RegExp(`^Assign$`, 'i') })
-      .first()
-  }
-
-  if (await assignAction.isVisible()) {
-    await assignAction.click()
-    // Wait for the assign modal to appear
-    await page.getByRole('button', { name: 'Assign', exact: true }).click()
-  }
+  await assignAction.click()
+  // Wait for the assign modal to appear
+  await page.getByRole('button', { name: 'Assign', exact: true }).click()
 
   await expect(
     page.getByRole('button', { name: 'Assign record' })
