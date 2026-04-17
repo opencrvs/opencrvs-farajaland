200a93bd0bdee39ea6fa7df55ad6ce10cbe718f2
pankaj-pant <44195004+pankaj-pant@users.noreply.github.com>
2026-04-17 11:02:43 +0300

    Add new cause of death form inputs in Farajaland from SPC implementation (#2093)
    * update death eventDetails
    
    * add config for death causeOfDeathDetails
    
    * update toolkit
    
    * clean up causeOfDeathDetails config
    
    * add reference_data icd10
    
    * test e2e
    
    * update e2e deploy
    
    * make setting up and seeding reference data conditional
    
    * add dashboard.view scope
    
    * update toolkit
    
    * fix e2e death test cases
    
    * fix e2e death test cases
    
    * update toolkit
    
    * update toolkit

--- src/index.ts
+++ src/index.ts
@@ -86,6 +86,8 @@ import { getClient } from './analytics/postgres'
 import { createClient } from '@opencrvs/toolkit/api'
 import { getGovernmentPortalApiRoutes } from './government-portal-api/routes'
 import { Event } from './events/utils/types'
+import { syncReferenceData } from './data-seeding/reference-data/reference-data'
+import { causeOfDeathSearchHandler } from './data-seeding/reference-data/handler'
 
 export interface ITokenPayload {
   sub: string
@@ -547,6 +549,17 @@ export async function createServer() {
   })
 
   server.route({
+    method: 'GET',
+    path: '/causes-of-death',
+    handler: causeOfDeathSearchHandler,
+    options: {
+      auth: false,
+      tags: ['api', 'search'],
+      description: 'Fuzzy search codes with source-based priority'
+    }
+  })
+
+  server.route({
     method: 'POST',
     path: `/trigger/events/{event}/actions/${ActionType.CUSTOM}`,
     handler: onCustomActionHandler,
@@ -776,6 +789,10 @@ export async function createServer() {
     if (env.ANALYTICS_DATABASE_URL) {
       await syncLocationLevels()
       await syncLocationStatistics()
+    }
+
+    if (env.REFERENCE_DATA_DATABASE_URL) {
+      await syncReferenceData()
     }
 
     logger.info(
