00b83975b69f2dac2c9427879732088701540ccc
Md. Ashikul Alam <32668488+Nil20@users.noreply.github.com>
2026-04-24 13:41:52 +0600

    fix: update default value of addresses (#2115)
    * fix: update default value of addresses
    
    * chore: add new test case covering address-less notify event
    
    * chore: reduce test steps
    
    * fix: update e2e tests with default address
    
    * fix: amend mother address
    
    * fix: amend default value of child birth location

--- src/events/birth/forms/pages/father.ts
+++ src/events/birth/forms/pages/father.ts
@@ -491,7 +491,7 @@ export const father = defineFormPage({
       defaultValue: {
         country: 'FAR',
         addressType: AddressType.DOMESTIC,
-        administrativeArea: user('primaryOfficeId').locationLevel('village')
+        administrativeArea: user('administrativeAreaId')
       },
       configuration: {
         streetAddressForm: defaultStreetAddressConfiguration
