3ad51bd65979714e0fdba8374f0156d60ed6a6e3
Markus <markus@agilepursuit.fi>
2026-06-04 11:48:34 +0300

    fix: assign on action menu for less flakiness

--- e2e/testcases/application/workqueue-flow-3.spec.ts
+++ e2e/testcases/application/workqueue-flow-3.spec.ts
@@ -235,10 +235,12 @@ test.describe.serial('3. Workqueue flow - 3', () => {
 
   test.describe('3.3 Re-declare and validate by RO', async () => {
     test('3.3.1 Go to edit', async () => {
-      await ensureAssignedFromWorkqueue(page, childName)
       await getRowByTitle(page, childName)
         .getByRole('button', { name: 'Review' })
         .click()
+
+      await ensureAssignedToUser(page, CREDENTIALS.REGISTRATION_OFFICER)
+
       await selectAction(page, 'Edit')
     })
 
