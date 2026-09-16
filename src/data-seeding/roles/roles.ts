cf332cf7f789010717e063937692d86699fe9748
Venceslas Burongu <bvenceslas@gmail.com>
2026-09-16 09:55:12 +0200

    disable printing certificate for other events than birth, death, marriage and divorce

--- src/data-seeding/roles/roles.ts
+++ src/data-seeding/roles/roles.ts
@@ -69,7 +69,7 @@ export const roles: Role[] = [
       { type: 'record.archive', options: { declaredIn: 'administrativeArea' } },
       { type: 'record.review-duplicates', options: { placeOfEvent: 'administrativeArea' } },
       { type: 'record.register', options: { placeOfEvent: 'administrativeArea' } },
-      { type: 'record.print-certified-copies', options: { registeredIn: 'administrativeArea' } },
+      { type: 'record.print-certified-copies', options: { registeredIn: 'administrativeArea', event: ['birth', 'death', 'marriage', 'divorce'] } },
       { type: 'record.correct', options: { registeredIn: 'administrativeArea', event: ['birth', 'death'] } },
       { type: 'record.custom-action', options: { event: ['birth'], customActionTypes: ['ESCALATE', 'REINSTATE_REVOKE_REGISTRATION'], placeOfEvent: 'administrativeArea' } },
       { type: 'record.custom-action', options: { event: ['birth'], customActionTypes: ['ISSUE_CERTIFIED_COPY', 'ISSUE_VERIFIABLE_CREDENTIAL'], registeredIn: 'administrativeArea' } },
